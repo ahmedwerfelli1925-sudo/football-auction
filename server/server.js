472f199618aa42431020feb67ab791476eb0739b
ahmedwerfelli1925-sudo <ahmedwerfelli1925@gmail.com>
2026-09-01 09:53:38 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,79 +28,74 @@ io.on("connection", (socket) => {
 
 
     // إنشاء غرفة
-    socket.on("createRoom", () => {
+    socket.on("createRoom", (playerData) => {
 
-        const roomCode =
-            Math.random()
-                .toString(36)
-                .substring(2, 7)
-                .toUpperCase();
+    const roomCode =
+        Math.random()
+            .toString(36)
+            .substring(2, 7)
+            .toUpperCase();
 
+    socket.join(roomCode);
 
-        socket.join(roomCode);
+    socket.data.playerName = playerData.name;
+    socket.data.roomCode = roomCode;
 
-
-        socket.emit("roomCreated", {
-            roomCode: roomCode
-        });
-
-
-        console.log(
-            "Room created:",
-            roomCode
-        );
-
+    socket.emit("roomCreated", {
+        roomCode: roomCode,
+        playerName: playerData.name
     });
+
+    console.log(
+        "Room created:",
+        roomCode,
+        "by",
+        playerData.name
+    );
+});
 
 
     // دخول غرفة
-    socket.on("joinRoom", (roomCode) => {
+   socket.on("joinRoom", (roomCode) => {
 
-        const room =
-            io.sockets.adapter.rooms.get(roomCode);
+    roomCode = roomCode.toUpperCase();
 
+    const room =
+        io.sockets.adapter.rooms.get(roomCode);
 
-        if (!room) {
+    if (!room) {
 
-            socket.emit("joinError", {
-                message: "الغرفة غير موجودة"
-            });
-
-            return;
-
-        }
-
-
-        if (room.size >= 2) {
-
-            socket.emit("joinError", {
-                message: "الغرفة ممتلئة"
-            });
-
-            return;
-
-        }
-
-
-        socket.join(roomCode);
-
-
-        socket.emit("roomJoined", {
-            roomCode: roomCode
+        socket.emit("joinError", {
+            message: "الغرفة غير موجودة!"
         });
 
+        return;
+    }
 
-        socket.to(roomCode).emit(
-            "playerJoined"
-        );
+    if (room.size >= 2) {
 
+        socket.emit("joinError", {
+            message: "الغرفة ممتلئة!"
+        });
 
-        console.log(
-            "Player joined room:",
-            roomCode
-        );
+        return;
+    }
 
+    socket.join(roomCode);
+
+    socket.data.roomCode = roomCode;
+
+    socket.emit("roomJoined", {
+        roomCode: roomCode
     });
+
+    socket.to(roomCode).emit("playerJoined");
+
+    console.log(
+        "Player joined room:",
+        roomCode
+    );
+});
 
 
     // خروج اللاعب
