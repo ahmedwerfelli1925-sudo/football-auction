301a4de98563472350a8ba8e9f6744e077d516fa
ahmedwerfelli1925-sudo <ahmedwerfelli1925@gmail.com>
2026-09-01 09:29:20 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,24 +14,116 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 3000;
 
+
+// الصفحة الرئيسية للسيرفر
 app.get("/", (req, res) => {
     res.send("⚽ Football Auction Server is Online!");
 });
 
+
+// عندما يدخل لاعب
 io.on("connection", (socket) => {
 
     console.log("Player connected:", socket.id);
 
-    socket.emit("connected", {
-        message: "Welcome to Football Auction!"
+
+    // إنشاء غرفة
+    socket.on("createRoom", () => {
+
+        const roomCode =
+            Math.random()
+                .toString(36)
+                .substring(2, 7)
+                .toUpperCase();
+
+
+        socket.join(roomCode);
+
+
+        socket.emit("roomCreated", {
+            roomCode: roomCode
+        });
+
+
+        console.log(
+            "Room created:",
+            roomCode
+        );
+
     });
 
+
+    // دخول غرفة
+    socket.on("joinRoom", (roomCode) => {
+
+        const room =
+            io.sockets.adapter.rooms.get(roomCode);
+
+
+        if (!room) {
+
+            socket.emit("joinError", {
+                message: "الغرفة غير موجودة"
+            });
+
+            return;
+
+        }
+
+
+        if (room.size >= 2) {
+
+            socket.emit("joinError", {
+                message: "الغرفة ممتلئة"
+            });
+
+            return;
+
+        }
+
+
+        socket.join(roomCode);
+
+
+        socket.emit("roomJoined", {
+            roomCode: roomCode
+        });
+
+
+        socket.to(roomCode).emit(
+            "playerJoined"
+        );
+
+
+        console.log(
+            "Player joined room:",
+            roomCode
+        );
+
+    });
+
+
+    // خروج اللاعب
     socket.on("disconnect", () => {
-        console.log("Player disconnected:", socket.id);
+
+        console.log(
+            "Player disconnected:",
+            socket.id
+        );
+
     });
 
 });
 
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+server.listen(
+    PORT,
+    "0.0.0.0",
+    () => {
+
+        console.log(
+            `Server running on port ${PORT}`
+        );
+
+    }
+);
